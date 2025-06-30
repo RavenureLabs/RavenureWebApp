3473f4c0dd2813dae852a1ff8c4079fb9184ec42
Weesli <78960827+Weesli@users.noreply.github.com>
2025-06-30 16:48:07 +0300

    added category controller and routes

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,42 @@
+import { getCategories, createCategory, updateCategory, deleteCategory, getCategoryById } from "@/src/controllers/category.controller";
+import { requireAuth } from "@/src/lib/middleware/auth";
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(request: NextRequest) {
+    const { action, data } = await request.json();
+    switch(action){
+        case 'getCategories':
+            return getCategories(data);
+        case 'createCategory':
+            const authCreateCategory = await requireAuth(request, ["admin"]);
+            if (authCreateCategory instanceof NextResponse) {
+                return authCreateCategory;
+            }
+            const userCreateCategory = authCreateCategory.user;
+            if (userCreateCategory.role !== 'admin') {
+                return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+            }
+            return createCategory(data);
+        case 'updateCategory':
+            const authUpdateCategory = await requireAuth(request, ["admin"]);
+            if (authUpdateCategory instanceof NextResponse) {
+                return authUpdateCategory;
+            }
+            const userUpdateCategory = authUpdateCategory.user;
+            if (userUpdateCategory.role !== 'admin') {
+                return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+            }
+            return updateCategory(data);
+        case 'deleteCategory':
+                const authDeleteCategory = await requireAuth(request, ["admin"]);
+                if (authDeleteCategory instanceof NextResponse) {
+                    return authDeleteCategory;
+                }
+                const userDeleteCategory = authDeleteCategory.user;
+                if (userDeleteCategory.role !== 'admin') {
+                    return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+                }
+                return deleteCategory(data);
+        default:
+            return NextResponse.json({ message: "Invalid action" }, { status: 400 });
+    }
+}
---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,72 @@
+import { NextResponse } from "next/server";
+import { connectToDatabase } from "../lib/database";
+import Category from "../models/category.model";
+export async function getCategories(data: any) {
+    await connectToDatabase();
+    try{
+        const categories = await Category.find();
+        return NextResponse.json(categories.map(category => {
+            return convertToDto(category);
+        }), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error fetching categories:", error);
+        return NextResponse.json({ message: "Error fetching categories", error }, { status: 500 });
+    }
+}
+export async function getCategoryById(data: any) {
+    await connectToDatabase();
+    try{
+        const category = await Category.findById(data.id);
+        return NextResponse.json(convertToDto(category), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error fetching category:", error);
+        return NextResponse.json({ message: "Error fetching category", error }, { status: 500 });
+    }
+}
+export async function createCategory(data: any) {
+    await connectToDatabase();
+    try{
+        const category = await Category.create(data);
+        return NextResponse.json(convertToDto(category), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error creating category:", error);
+        return NextResponse.json({ message: "Error creating category", error }, { status: 500 });
+    }
+}
+export async function updateCategory(data: any) {
+    await connectToDatabase();
+    try{
+        const category = await Category.findByIdAndUpdate(data.id, data, { new: true });
+        return NextResponse.json(convertToDto(category), { status: 200 });
+    }
+    catch (error) {
+        console.error("Error updating category:", error);
+        return NextResponse.json({ message: "Error updating category", error }, { status: 500 });
+    }
+}
+export async function deleteCategory(data: any) {
+    await connectToDatabase();
+    try{
+        const category = await Category.findByIdAndDelete(data.id);
+        return NextResponse.json(convertToDto(category), { status: 200 });
+    }
+    catch (
+        error) {
+        console.error("Error deleting category:", error);
+        return NextResponse.json({ message: "Error deleting category", error }, { status: 500 });
+    }
+}
+
+const  convertToDto = (category: any) => {
+    return {
+        id: category._id,
+        name: category.name,
+        products: category.products,
+        createdAt: category.createdAt,
+        updatedAt: category.updatedAt,
+        isActive: category.isActive,
+    };
+};
