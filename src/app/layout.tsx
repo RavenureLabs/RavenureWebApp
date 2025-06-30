45220f560776000b63615d2db24035fd897ff92c
Weesli <78960827+Weesli@users.noreply.github.com>
2025-06-30 16:35:57 +0300

    added product controller and router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import {Poppins } from "next/font/google";
-=======
->>>>>>> 90baf45e452f2cf101e5ef89d6842496422cf90e
 import "./globals.css";
 import ClientLayout from "../components/layout/client.layout"; 
-import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
   subsets: ['latin'],
-<<<<<<< HEAD
   weight: ['400', '600', '700'], 
-=======
-  weight: ['400', '600', '700'], // istediğin ağırlıklar
->>>>>>> 90baf45e452f2cf101e5ef89d6842496422cf90e
   display: 'swap',
 });
 
@@ -30,11 +22,7 @@ export default function RootLayout({
         />
         <link rel="icon" href="logo.png" type="image/png" />
       </head>
-<<<<<<< HEAD
       <body className={`${poppins.className} antialiased`}>
-=======
-      <body className={`${poppins.className}`}>
->>>>>>> 90baf45e452f2cf101e5ef89d6842496422cf90e
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
