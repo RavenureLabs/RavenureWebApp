5f55e923292325efda00e3b98537b6d021741c7b
Weesli <78960827+Weesli@users.noreply.github.com>
2025-07-08 14:23:50 +0300

    Referans bileşeni eklendi ve referansları dinamik olarak yüklemek için güncellemeler yapıldı

--- src/components/pages/referances.page.tsx
+++ src/components/pages/referances.page.tsx
@@ -1,8 +1,21 @@
 'use client';
 
-import { ArrowRight } from 'lucide-react';
+import { referanceService } from '@/src/lib/services';
+import { ReferanceType } from '@/src/models/referance.model';
+import { useEffect, useState } from 'react';
+import ReferanceComponent from '../referance/referance.component';
 
 export default function ReferencesPageComponent() {
+  const [referances, setReferances] = useState<ReferanceType[]>([]);
+
+  useEffect(() => {
+    const fetchReferances = async () => {
+      const referances = await referanceService.getReferances();
+      setReferances(referances);
+    };
+    fetchReferances();
+  }, []);
+
   return (
     <div className="min-h-screen bg-zinc-950 text-white py-16 px-4">
       <div className="max-w-5xl mx-auto text-center mb-12">
@@ -15,23 +28,13 @@ export default function ReferencesPageComponent() {
       </div>
 
       <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4">
-        {/* Referans 1 */}
-        <div className="bg-zinc-900 border border-zinc-800 rounded-xl p-6 flex flex-col items-center text-center hover:scale-[1.02] transition">
-          <img src="/Ravenure-Logo.png" alt="Mythonia" className="w-16 h-16 object-contain mb-4" />
-          <h3 className="text-xl font-semibold text-white">Ravenure</h3>
-        </div>
-
-        {/* Referans 2 */}
-        <div className="bg-zinc-900 border border-zinc-800 rounded-xl p-6 flex flex-col items-center text-center hover:scale-[1.02] transition">
-          <img src="/Ravenure-Logo.png" alt="SkyForge" className="w-16 h-16 object-contain mb-4" />
-          <h3 className="text-xl font-semibold text-white">Ravenure</h3>
-        </div>
-
-        {/* Referans 3 */}
-        <div className="bg-zinc-900 border border-zinc-800 rounded-xl p-6 flex flex-col items-center text-center hover:scale-[1.02] transition">
-          <img src="/Ravenure-Logo.png" alt="BlockRealm" className="w-16 h-16 object-contain mb-4" />
-          <h3 className="text-xl font-semibold text-white">Ravenure</h3>
-        </div>
+        {
+          referances.map((referance, index) => (
+            <ReferanceComponent 
+            referance={referance}
+            />
+          ))
+        }
       </div>
     </div>
   );
