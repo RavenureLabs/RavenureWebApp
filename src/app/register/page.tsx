2258aa54b926d61a3b4a28e50b706776eb7e2531
Weesli <78960827+Weesli@users.noreply.github.com>
2025-07-10 14:55:34 +0300

    register page setup

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterPageComponent from "@/src/components/pages/register.page";
+
+export const metadata = {
+  title: "Register Page",
+  description: "Register Page",
+};
+
+export default function RegisterPage() {
+  return (
+    <RegisterPageComponent />
+  );
+}
