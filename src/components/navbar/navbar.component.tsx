7280eb20815a439c2ce6a5c314dd298b8afdf32d
Weesli <78960827+Weesli@users.noreply.github.com>
2025-07-18 12:38:19 +0300

    .

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -3,10 +3,12 @@ import { UserIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 import { useLanguage } from '@/src/hooks/uselanguage.hooks';
 import { config } from '@/src/config/config';
 import { useSession } from 'next-auth/react';
+import { useCartStore } from '@/src/stores/cart.store';
 
 export default function Header() {
   const { text } = useLanguage();
   const {data:session} = useSession();
+  const {toggle} = useCartStore();
 
   return (
     <header className="sticky top-0 z-50 bg-[#0f0f10] text-white border-b border-gray-700 border-opacity-40 backdrop-blur">
@@ -46,7 +48,7 @@ export default function Header() {
 
         {/* İkonlar */}
         <div className="flex items-center gap-2">
-          <a href="/cart" className="relative p-2  transition-all duration-300 overflow-hidden group">
+          <a onClick={toggle} className="relative p-2  transition-all duration-300 overflow-hidden group">
             <span className="relative z-10"><ShoppingCartIcon className="w-5 h-5" /></span>
             <span className="absolute inset-0 rounded-[20px] bg-gradient-to-r from-white/10 to-transparent opacity-0 group-hover:opacity-100 transition-all duration-300" />
           </a>
